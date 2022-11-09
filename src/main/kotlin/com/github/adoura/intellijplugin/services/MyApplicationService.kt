b3dc0087680fb4d6b087fcf3df1854dce6435510
GitHub Action <action@github.com>
2022-11-09 09:39:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/adoura/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adoura/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adoura.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adoura.intellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/adoura/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adoura/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adoura.intellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adoura.intellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
